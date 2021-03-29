03be8255974732cf2c097a7690a28abb6472ca8b
lingdev1998 <linhdeptrai1029i@gmail.com>
2021-03-30 02:56:11 +0700

    add: song-option

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,11 +3,13 @@ import SideBar from "../components/SideBar";
 import PageWrapper from "../components/PageWrapper";
 import Footer from "../components/Footer";
 import Player from "../components/Player";
+import Header from "../components/Header";
 
 const LayoutHome = (props) => {
   return (
     <div className="ms_main_wrapper">
       <SideBar />
+      <Header/>
       <PageWrapper />
       <Footer />
       <Player />
