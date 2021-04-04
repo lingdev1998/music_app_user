075d53d95f27fb2ba8e8f4ac603d48dbf9ff3bd6
lingdev1998 <linhdeptrai1029i@gmail.com>
2021-04-05 05:01:41 +0700

    remove file

---
+++ src/redux/reducers/appReducer.js
@@ -0,0 +1,64 @@
+export const ACTION_TYPES = {
+  CHANGING_NAVBAR_ROUTE: "CHANGING_NAVBAR_ROUTE",
+  CHANGED_NAVBAR_ROUTE: "CHANGED_NAVBAR_ROUTE",
+  SHOW_LOGIN_MODAL: "SHOW_LOGIN_MODAL",
+  CLOSE_LOGIN_MODAL: "CLOSE_LOGIN_MODAL",
+  SHOW_LOADING: "SHOW_LOADING",
+  STOP_SHOW_LOADING: "STOP_SHOW_LOADING",
+};
+
+export const setShowLoginModal = () => {
+  return {
+    type: ACTION_TYPES.SHOW_LOGIN_MODAL,
+  };
+};
+
+export const setCloseLoginModal = () => ({ type: ACTION_TYPES.CLOSE_LOGIN_MODAL });
+
+export const showLoading = () => ({ type: ACTION_TYPES.SHOW_LOADING });
+
+export const stopShowLoading = () => ({ type: ACTION_TYPES.STOP_SHOW_LOADING });
+
+const initState = {
+  loading: false,
+  loginModalShow: false,
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.CHANGING_NAVBAR_ROUTE:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ACTION_TYPES.CHANGED_NAVBAR_ROUTE:
+      return {
+        ...state,
+        loading: false,
+      };
+    case ACTION_TYPES.SHOW_LOGIN_MODAL:
+      return {
+        ...state,
+        loginModalShow: true,
+      };
+    case ACTION_TYPES.CLOSE_LOGIN_MODAL:
+      return {
+        ...state,
+        loginModalShow: false,
+      };
+    case ACTION_TYPES.SHOW_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case ACTION_TYPES.STOP_SHOW_LOADING:
+      return {
+        ...state,
+        isLoading: false,
+      };
+    default:
+      return { ...state };
+  }
+};
+
+export default reducer;
